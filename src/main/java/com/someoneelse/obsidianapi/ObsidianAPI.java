e6c067591eaceb5020eae1eb733fd61b34c37d5a
ItsRemurin <ItsRemurin@gmail.com>
2023-10-03 17:00:28 -0400

    Initial commit

---
+++ src/main/java/com/someoneelse/obsidianapi/ObsidianAPI.java
@@ -0,0 +1,15 @@
+package com.someoneelse.obsidianapi;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+@Mod(ObsidianAPI.MODID)
+public class ObsidianAPI
+{
+    public static final String MODID = "obsidianapi";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ObsidianAPI() {
+
+    }
+}
